be2a88bdfee04c8296b2f4eca668726ae2150ba6
Vitalii Baz <baz.vitalii@gmail.com>
2015-11-20 17:01:35 +0200

    first commit

---
+++ task01.py
@@ -0,0 +1,23 @@
+# Создать класс Person, описывающий человека. Конструктор принимает параметрами
+# имя и возраст и сохраняет себе эту информацию в классе. Также создать функцию say,
+# которая не принимает параметров, печатает на экран
+# "Hello, my name is <??> and I am <??> years old.", ничего не возвращает.
+# Для демонстрации, работы, инициализировать две персоны Alice и Bob,
+# и вызвать функцию say для каждого из них. Результат запуска программы будет две строчки,
+# напечатанные в консоль.
+
+class Person(object):
+	def __init__(self, name, age):
+        super(Person, self).__init__()
+        self.name = name
+        self.age = age
+
+    def say(self):
+    	print("Hello, my name is {0} and I am {1} years old.".format(self.name, self.age))
+
+
+alice = Person('Alice', '18')
+bob = Person('Bob', '81')
+
+say(alice)
+say(bob)
